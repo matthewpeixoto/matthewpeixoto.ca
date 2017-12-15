d7c337678524e6b78cc233807f9e6b16da283c03
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-12-15 09:47:31 -0500

    Fix the GeoMattric animations

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,15 @@
+var $smIntroPhotos = [$('.sm-section-intro-image-user-experience'), $('.sm-section-intro-image-personas'), $('.sm-section-intro-image-visual-design'), $('.sm-section-intro-image-ideation')];
+
+$smIntroPhotos.forEach(function (smIntroPhoto) {
+  smIntroPhoto.waypoint(function (direction) {
+    if (direction == 'down') {
+      smIntroPhoto.addClass('js-sm-section-intro-image-animate');
+    } else {
+      smIntroPhoto.removeClass('js-sm-section-intro-image-animate');
+    }
+  }, {offset: '75%'});
+});
+
 var $geomattricShapes = [$('.modular-system'), $('.friendly-curves-1'), $('.friendly-curves-2'), $('.geometric-forms-2'), $('.geometric-forms-3'), $('.geometric-forms-4'), $('.geometric-forms-5'), $('.geometric-forms-6'), $('.geometric-forms-7')];
 var $geomattricShift = $('.js-geomattric-shift');
 var $win = $(window);
